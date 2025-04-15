e445dafb856199669c35deaca3065cd109573ddf
Brian Tomlinson <darthlukan@gmail.com>
2025-04-14 22:01:45 -0700

    Fix typo

--- chatty.scm
+++ chatty.scm
@@ -2,7 +2,7 @@
 !#
 
 (define (chatty-add chatty-name . nums)
-  (format #t "<~a> If you add those togethe you get ~a!\n"
+  (format #t "<~a> If you add those together you get ~a!\n"
           chatty-name (apply + nums)))
 
 (chatty-add "Chester" 2 4 8 6)
